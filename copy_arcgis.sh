3c94fd50a271a12a14d38066eef75a354d17787b
John Ericksen <johncarl81@gmail.com>
2021-03-28 18:47:51 -0600

    Added copy_arcgis utility

---
+++ copy_arcgis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /home/john/.gradle/caches/modules-2/files-2.1/com.esri.arcgisruntime/arcgis-java-jnilibs/100.10.0/9e369acaf35d3147d7951239a20df390bf65af0f/* docker/
+cp /home/john/.gradle/caches/modules-2/files-2.1/com.esri.arcgisruntime/arcgis-java-resources/100.10.0/74a8b97056fe36fcd6ea07dd807d2da282a30362/* docker/
