68569dc60dbdb362f6043db665647b0ce6565362
John Ericksen <johncarl81@gmail.com>
2021-03-28 19:51:12 -0600

    Remove second rosbridge startup

--- dragonflyHost.sh
+++ dragonflyHost.sh
@@ -2,10 +2,9 @@
 
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 wlan1
 roscore&
-roslaunch rosbridge_server rosbridge_websocket.launch &
-sleep 10
+sleep 3
 rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.0 &
-sleep 10
+sleep 3
 rosrun master_sync_fkie master_sync &
-sleep 10
+sleep 3
 roslaunch rosbridge_server rosbridge_websocket.launch&
