08a42ce11fabcc4b0abe478e2c4642d1afaba4e4
John Ericksen <johncarl81@gmail.com>
2024-02-08 21:23:32 -0700

    Added co2 threshold pump to waypoint

--- src/main/java/edu/unm/dragonfly/mission/GotoCreator.java
+++ src/main/java/edu/unm/dragonfly/mission/GotoCreator.java
@@ -3,10 +3,10 @@ package edu.unm.dragonfly.mission;
 import edu.unm.dragonfly.mission.step.MissionStep;
 import edu.unm.dragonfly.mission.step.MissionStepGoto;
 import javafx.collections.FXCollections;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author John Ericksen
@@ -15,6 +15,9 @@ public class GotoCreator implements MissionStepCreator {
 
     private final ComboBox<String> droneSelection;
     private final ComboBox<String> waypointSelection;
+    private ComboBox<Integer> pumpNumber;
+    private CheckBox runPump;
+    private TextField pumpThreshold;
 
     public GotoCreator(List<String> drones, List<String> waypoints) {
         this.droneSelection = new ComboBox<>(FXCollections.observableList(drones));
@@ -23,14 +26,24 @@ public class GotoCreator implements MissionStepCreator {
 
     @Override
     public void create(GridPane grid) {
+        pumpNumber = new ComboBox<>(FXCollections.observableList(Arrays.asList(0, 1, 2, 3)));
+        runPump = new CheckBox();
+        pumpThreshold = new TextField();
 
         GridUtil.builder(grid).increment()
                 .add("Drone:", droneSelection)
-                .add("Waypoint:", waypointSelection);
+                .add("Waypoint:", waypointSelection)
+                .add("Pump CO2:", runPump)
+                .add("Pump number:", pumpNumber)
+                .add("Pump Threshold:", pumpThreshold);
     }
 
     @Override
     public MissionStep build() {
-        return new MissionStepGoto(droneSelection.getSelectionModel().getSelectedItem(), waypointSelection.getSelectionModel().getSelectedItem());
+        return new MissionStepGoto(droneSelection.getSelectionModel().getSelectedItem(),
+                waypointSelection.getSelectionModel().getSelectedItem(),
+                runPump.isSelected(),
+                pumpNumber.getSelectionModel().getSelectedItem(),
+                Double.parseDouble(pumpThreshold.getText()));
     }
 }
--- src/main/java/edu/unm/dragonfly/mission/step/MissionStepGoto.java
+++ src/main/java/edu/unm/dragonfly/mission/step/MissionStepGoto.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.unm.dragonfly.Fixture;
 import edu.unm.dragonfly.WaypointFixture;
 
+import java.util.*;
+
 /**
  * @author John Ericksen
  */
@@ -14,11 +16,21 @@ public class MissionStepGoto implements MissionStep {
 
     private final String drone;
     private final String waypoint;
+    private final boolean runPump;
+    private final int pumpNum;
+    private final double pumpThreshold;
 
     @JsonCreator
-    public MissionStepGoto(@JsonProperty("drone") String drone, @JsonProperty("waypoint") String waypoint) {
+    public MissionStepGoto(@JsonProperty("drone") String drone,
+                           @JsonProperty("waypoint") String waypoint,
+                           @JsonProperty("runPump") boolean runPump,
+                           @JsonProperty("pumpNum") int pumpNum,
+                           @JsonProperty("pumpThreshold") double pumpThreshold) {
         this.drone = drone;
         this.waypoint = waypoint;
+        this.runPump = runPump;
+        this.pumpNum = pumpNum;
+        this.pumpThreshold = pumpThreshold;
     }
 
     @Override
@@ -39,6 +51,9 @@ public class MissionStepGoto implements MissionStep {
         ObjectNode data = gotoWaypoint.putObject("goto_step");
 
         data.put("waypoint", waypoint);
+        data.put("run_pump", runPump);
+        data.put("pump_threshold", pumpThreshold);
+        data.put("pump_num", pumpNum);
 
         return gotoWaypoint;
     }
@@ -51,6 +66,18 @@ public class MissionStepGoto implements MissionStep {
         return waypoint;
     }
 
+    public boolean isRunPump() {
+        return runPump;
+    }
+
+    public int getPumpNum() {
+        return pumpNum;
+    }
+
+    public double getPumpThreshold() {
+        return pumpThreshold;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -58,22 +85,34 @@ public class MissionStepGoto implements MissionStep {
 
         MissionStepGoto that = (MissionStepGoto) o;
 
-        if (drone != null ? !drone.equals(that.drone) : that.drone != null) return false;
-        return waypoint != null ? waypoint.equals(that.waypoint) : that.waypoint == null;
+        if (runPump != that.runPump) return false;
+        if (pumpNum != that.pumpNum) return false;
+        if (Double.compare(pumpThreshold, that.pumpThreshold) != 0) return false;
+        if (!Objects.equals(drone, that.drone)) return false;
+        return Objects.equals(waypoint, that.waypoint);
     }
 
     @Override
     public int hashCode() {
-        int result = drone != null ? drone.hashCode() : 0;
+        int result;
+        long temp;
+        result = drone != null ? drone.hashCode() : 0;
         result = 31 * result + (waypoint != null ? waypoint.hashCode() : 0);
+        result = 31 * result + (runPump ? 1 : 0);
+        result = 31 * result + pumpNum;
+        temp = Double.doubleToLongBits(pumpThreshold);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
 
     @Override
     public String toString() {
-        return "Goto{" +
+        return "MissionStepGoto{" +
                 "drone='" + drone + '\'' +
                 ", waypoint='" + waypoint + '\'' +
+                ", runPump=" + runPump +
+                ", pumpNum=" + pumpNum +
+                ", pumpThreshold=" + pumpThreshold +
                 '}';
     }
 }
